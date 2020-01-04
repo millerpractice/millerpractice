edcd4c92407354a3925abb72ccd6eedbf064ff19
millerpractice <sarahla@abv.bg>
2020-01-04 11:17:32 +0100

    Updates

---
+++ precache-manifest.fb178b45f7ede5e2387269fb1f00e8b0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbf177c63a5ea1f8ad85e900849c7da",
+    "url": "/millerpractice/index.html"
+  },
+  {
+    "revision": "f60e9dcfeb39bf4c3a79",
+    "url": "/millerpractice/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8dd566742cda42da6ded",
+    "url": "/millerpractice/static/js/2.a408834a.chunk.js"
+  },
+  {
+    "revision": "a0560e08c2bea09cecbe463a56dcf52b",
+    "url": "/millerpractice/static/js/2.a408834a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f60e9dcfeb39bf4c3a79",
+    "url": "/millerpractice/static/js/main.50693050.chunk.js"
+  },
+  {
+    "revision": "c5e34cd062f964f76c91",
+    "url": "/millerpractice/static/js/runtime-main.6baaa09a.js"
+  },
+  {
+    "revision": "c840e6103f9c993f09b8257236360298",
+    "url": "/millerpractice/static/media/female.c840e610.png"
+  },
+  {
+    "revision": "3f35d1394483ca050ff7cbf34b177359",
+    "url": "/millerpractice/static/media/healthcare.3f35d139.png"
+  },
+  {
+    "revision": "0f807958568a1a23afd33663356e317d",
+    "url": "/millerpractice/static/media/hearth.0f807958.png"
+  },
+  {
+    "revision": "692727752824ed1f880389f1bbc5fd44",
+    "url": "/millerpractice/static/media/logo.69272775.png"
+  },
+  {
+    "revision": "b34c85bdeb564a07942c3bc651886f2c",
+    "url": "/millerpractice/static/media/map.b34c85bd.PNG"
+  },
+  {
+    "revision": "9c84c66ca647ca56c7a35f09efb55aca",
+    "url": "/millerpractice/static/media/office.9c84c66c.jpg"
+  },
+  {
+    "revision": "de578f1b6cbbb6ff9ae136a6cf0000b3",
+    "url": "/millerpractice/static/media/profile doc transperent.de578f1b.png"
+  },
+  {
+    "revision": "df75fc928499f2301dff24d608c573ea",
+    "url": "/millerpractice/static/media/welcome.df75fc92.jpg"
+  }
+]);
